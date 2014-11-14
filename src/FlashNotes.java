2f90954fa89ca90433396aa7bcba056d0754fff1
Ben Coble <ben.j.coble@gmail.com>
2014-11-14 14:15:54 -0500

    Improved pretty formatter and tweaked bio identifier.

--- src/FlashNotes.java
+++ src/FlashNotes.java
@@ -53,12 +53,127 @@ public class FlashNotes {
 			List<List<CoreLabel>> out = classifier.classify(fileContents);
 
 			// Run the summarizer
-			ArrayList<String> results = summarize(out, length_of_summary, fileContents);
-			
+			ArrayList<String> results = summarize(out, length_of_summary,
+					fileContents);
+
 			// Write results to designated file
-			writeResults(results, outfile);		
+			writeResults(results, outfile);
 
 		}
+	}
+
+	/**
+	 * Performs the summarizing function on a text that has been classified.
+	 * 
+	 * @param text
+	 * @param cap
+	 * @param doc
+	 * @return
+	 */
+	public static ArrayList<String> summarize(List<List<CoreLabel>> text,
+			int cap, String doc) {
+
+		ArrayList<List<CoreLabel>> summary = new ArrayList<List<CoreLabel>>();
+
+		// Finds who the bio is about.
+		// TODO - make it only count ner's of type PERSON
+		String bio_person = "Harry Potter";
+		int current_count = 0;
+		for (List<CoreLabel> sentence : text) {
+			for (CoreLabel word : sentence) {
+				int freq = 0;
+				String ner = word.word();
+				String wClass = word
+						.get(CoreAnnotations.AnswerAnnotation.class);
+				if (wClass.equals("PERSON")) {
+					freq = countTerm(ner, doc);
+					if (freq > current_count) {
+						bio_person = ner;
+					}
+				}
+			}
+		}
+		String pregen = "This is a biography about " + bio_person + ".";
+
+		summary.addAll(text);
+
+		// booleans to control how "harsh" the summarizer is
+		boolean ner2_done = true;
+		boolean no_quotes = false;
+		boolean only_dates = false;
+		int iterations = 0;
+
+		// Loops until summary is under the cap
+		while (summary.size() > cap) {
+			iterations++;
+			System.out.println("Iteration: " + iterations);
+			ArrayList<List<CoreLabel>> temp = new ArrayList<List<CoreLabel>>();
+
+			for (List<CoreLabel> sentence : summary) {
+				int nerCount = 0;
+				boolean has_date = false;
+				boolean is_quote = false;
+
+				for (CoreLabel word : sentence) {
+					String ner = word.word();
+					String wClass = word
+							.get(CoreAnnotations.AnswerAnnotation.class);
+
+					// Set booleans based on terms located in phrase
+					if (!wClass.equals("O")) {
+						nerCount++;
+					}
+					if (wClass.equals("DATE")) {
+						has_date = true;
+					}
+					if (ner.equals("``")) {
+						is_quote = true;
+					}
+
+				}
+				// boolean checks - this decides if a phrase is included in the
+				// summary
+				if (ner2_done) { // ner 2 check
+					if (nerCount >= 2) {
+						if (no_quotes) { // quotes check
+							if (!is_quote) {
+								if (only_dates) { // dates check
+									if (has_date) {
+										temp.add(sentence);
+									}
+								} else {
+									temp.add(sentence);
+								}
+							}
+						} else {
+							temp.add(sentence);
+						}
+					}
+				}
+			}
+
+			summary = temp;
+
+			// Sets the next level of summarizing based on the current level
+			// ie. if ner2 is done but quotes haven't been removed and it is
+			// still over the cap, it will set that boolean for the next loop.
+			// Also breaks out if all the ways of summarizing are exhausted.
+			if (ner2_done) {
+				if (no_quotes) {
+					if (only_dates) {
+						break;
+					} else {
+						only_dates = true;
+					}
+				} else {
+					no_quotes = true;
+				}
+			}
+		}
+
+		ArrayList<String> results = new ArrayList<String>();
+		results = prettyFormatter(summary, pregen);
+		return results;
 	}
 
 	/**
@@ -69,14 +184,15 @@ public class FlashNotes {
 	 * @throws FileNotFoundException
 	 * @throws UnsupportedEncodingException
 	 */
-	public static void writeResults(ArrayList<String> text, String outfile) throws FileNotFoundException, UnsupportedEncodingException{
+	public static void writeResults(ArrayList<String> text, String outfile)
+			throws FileNotFoundException, UnsupportedEncodingException {
 		PrintWriter writer = new PrintWriter(outfile, "UTF-8");
-		for (String line: text){
+		for (String line : text) {
 			writer.println(line);
 		}
-		writer.close();		
+		writer.close();
 	}
-	
+
 	/**
 	 * Counts the number of occurrences of a term in the document.
 	 * 
@@ -100,119 +216,7 @@ public class FlashNotes {
 		return count;
 
 	}
-	
-	/**
-	 * Performs the summarizing function on a text that has been classified.
-	 * 
-	 * @param text
-	 * @param cap
-	 * @param doc
-	 * @return
-	 */
-	public static ArrayList<String> summarize(List<List<CoreLabel>> text, int cap, String doc){
 
-		ArrayList<List<CoreLabel>> summary = new ArrayList<List<CoreLabel>>();
-				
-		// Finds who the bio is about.
-		// TODO - make it only count ner's of type PERSON
-		String bio_person = "Harry Potter";
-		int current_count = 0;
-		for (List<CoreLabel> sentence : text) {
-			for (CoreLabel word : sentence) {
-				int freq = 0;
-				String ner = word.word();
-				String wClass = word
-						.get(CoreAnnotations.AnswerAnnotation.class);
-				if (!wClass.equals("O")) {
-					freq = countTerm(ner, doc);
-					if (freq > current_count){
-						bio_person = ner;
-					}
-				} 
-			}
-		}
-		String pregen = "This is a biography about " + bio_person + ".";
-
-		summary.addAll(text);
-		
-		// booleans to control how "harsh" the summarizer is
-		boolean ner2_done = true;
-		boolean no_quotes = false;
-		boolean only_dates = false;		
-		int iterations = 0;
-		
-		// Loops until summary is under the cap
-		while(summary.size() > cap){
-			iterations ++;
-			System.out.println("Iteration: " + iterations);
-			ArrayList<List<CoreLabel>> temp = new ArrayList<List<CoreLabel>>();
-			
-			for (List<CoreLabel> sentence : summary) {
-				int nerCount = 0;
-				boolean has_date = false;
-				boolean is_quote = false;
-				
-				for (CoreLabel word : sentence) {
-					String ner = word.word();
-					String wClass = word
-							.get(CoreAnnotations.AnswerAnnotation.class);
-					
-					// Set booleans based on terms located in phrase
-					if (!wClass.equals("O")){
-						nerCount++;
-					}
-					if (wClass.equals("DATE")){
-						has_date = true;
-					}
-					if (ner.equals("``")){
-						is_quote = true;
-					}
-					
-				}
-				// boolean checks - this decides if a phrase is included in the summary
-				if (ner2_done){ // ner 2 check
-					if (nerCount >= 2){
-						if (no_quotes){ // quotes check
-							if (!is_quote){
-								if (only_dates){ // dates check
-									if (has_date){
-										temp.add(sentence);
-									}
-								} else {
-									temp.add(sentence);
-								}
-							}
-						} else {
-							temp.add(sentence);
-						}
-					}
-				}
-			}
-			
-			summary = temp;
-			
-			// Sets the next level of summarizing based on the current level
-			// ie. if ner2 is done but quotes haven't been removed and it is
-			// still over the cap, it will set that boolean for the next loop.
-			// Also breaks out if all the ways of summarizing are exhausted.
-			if (ner2_done){
-				if (no_quotes){
-					if (only_dates){
-						break;
-					} else {
-						only_dates = true;
-					}
-				} else {
-					no_quotes = true;
-				}
-			}
-		}
-			
-		ArrayList<String> results = new ArrayList<String>();
-		results = prettyFormatter(summary, pregen);
-		return results;
-	}
-	
 	/**
 	 * Re-formats the summary for writing to a file
 	 * 
@@ -227,7 +231,8 @@ public class FlashNotes {
 		for (List<CoreLabel> sentence : summary){
 			String line = "";
 			for (CoreLabel word : sentence){
-				line += word.word() + " ";
+				String w = word.word();
+				line = w.equals(".") || w.equals(",") || w.equals("!") || w.equals("?") || w.equals("'") ? line + w : line + " " + w;
 			}
 			results.add(line);
 		}
